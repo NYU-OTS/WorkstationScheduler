dfea31cb22ed0460e27a3f847207eed89a53e846
Yiyang <674977156@qq.com>
2018-09-10 12:29:32 -0400

    Restructured the directories

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,20 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
-import registerServiceWorker from './registerServiceWorker';
+import * as React from "react";
+import { render } from "react-dom";
+import Hello from "./components/Hello";
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root') as HTMLElement
+const styles = {
+  fontFamily: "sans-serif",
+  textAlign: "center"
+};
+
+const App = () => (
+  <div style={styles}>
+    <Hello name="TypeScript" enthusiasmLevel={10} />
+    <h2>Start editing to see some magic happen {"\u2728"}</h2>
+  </div>
 );
-registerServiceWorker();
+
+render(
+  <App />, 
+  document.getElementById("root") as HTMLElement
+);
